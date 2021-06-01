555d8af390d9d143032b40ac5f7fbcfd2419ed75
NayKayvd <vandiemennahum@gmail.com>
2021-06-01 14:19:11 +0200

    updated

--- MLC2.py
+++ MLC2.py
@@ -1,9 +1,11 @@
+# Nahum Van Diemen
 import numpy as np
 import matplotlib.pyplot as plt
+
 nums = np.array([0.5, 0.7, 1.0, 1.2, 1.3, 2.1])
 bins = np.array([0, 1, 2, 3])
-print("nums: ",nums)
-print("bins: ",bins)
+print("nums: ", nums)
+print("bins: ", bins)
 print("Result:", np.histogram(nums, bins))
 plt.xlabel("numbers")
 plt.ylabel("bins")
